4a886f8c52ce5234d9ca70d4203959efc222c4d4
Isaac C. Aronson <i@pingas.org>
2022-02-17 17:13:46 -0600

    Update main to be valid

--- Program.fs
+++ Program.fs
@@ -1,3 +1,5 @@
-﻿[<EntryPoint>] let main args = printfn "Arguments passed to function : %A" args
+﻿[<EntryPoint>]
+let main args =
+    printfn "Arguments passed to function : %A" args
     // Return 0. This indicates success.
     0
